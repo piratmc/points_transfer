0c3af76ee05a78ecba52f493c1606d60e049d43c
Dmitry <dmitry@gnoper.com>
2024-06-11 14:06:10 -0400

    Search Etihad partners, cleaner code

--- converter.rb
+++ converter.rb
@@ -12,35 +12,12 @@
 # Air Mauritius
 # Avianca
 
-
-# Ethihad partners
-# Air Canada (AC)
-# Air Europa (UX)
-# Air France (AF)
-# Air New Zealand (NZ)
-# All Nippon Airways (NH)
-# Asiana Airlines (OZ)
-# Avianca (AV)
-# Bangkok Airways (PG)
-# FlexFlight (W2)
-# Belavia (B2)
-# brussels airlines (SN)
-# China Eastern (MU)
-# Egyptair (MS)
-# EL AL Airlines (LY)
-# Garuda Indonesia (GA)
-# Gulf Air (GF)
-# ITA Airways (AZ)
-# JetBlue (B6)
-# KLM (KL)
-# Korean Air (KE)
-# Lufthansa (LH)
-# Middle East Air (ME)
-# Royal Air Maroc (AT)
-# SWISS International Air Lines (LX)
-# Turkish Airlines (TK) 
-# Virgin Australia (VA)
-
+directPartners = {
+    'Etihad' => ['Air Canada', 'Air Europa', 'Air France KLM', 'Air New Zealand', 'All Nippon Airways', 'Asiana Airlines', 'Avianca',
+        'Bangkok Airways', 'FlexFlight', 'Belavia', 'Brussels airlines', 'China Eastern', 'Egyptair', 'EL AL Airlines', 'Garuda Indonesia',
+        'Gulf Air', 'ITA Airways', 'JetBlue', 'Korean Air', 'Lufthansa', 'Middle East Air', 'Royal Air Maroc', 'SWISS',
+        'Turkish Airlines', 'Virgin Australia']
+}
 
 alliances = {
     'OneWorld' => ['Alaska Airlines', 'American Airlines', 'British Airways', 'Cathay Pacific', 'Finnair', 'Iberia', 'Japan Airlines',
@@ -57,61 +34,81 @@ alliances = {
         'South African Airways','Swiss','TAP Air Portugal','Thai Airways International','Turkish Airlines','United']
 }
 
-ccPrograms = {
-    'CapitalOne' => ['Virgin Atlantic', 'TAP', 'Singapore Airlines', 'Qantas', 'Finnair', 'EVA Air', 'Ethihad', 'Emirates', 
+cards = {
+    'Capital One' => ['Virgin Atlantic', 'TAP', 'Singapore Airlines', 'Qantas', 'Finnair', 'EVA Air', 'Ethihad', 'Emirates', 
         'British Airways', 'Avianca', 'Cathay Pacific', 'All Accor', 'Air Canada', 'Aeromexico'],
         
     'Chase' => ['Aer Lingus', 'Air Canada', 'British Airways', 'Emirates', 'Air France KLM', 'Iberia', 'JetBlue',
         'Singapore Airlines', 'Southwest', 'United', 'Cathay Pacific', 'Virgin Atlantic']
 }
 
-flightAirline = gets.chomp()
-flightAlliance = false
+puts "What is your desired flight airline name?"
+flightAirline = gets.chomp().downcase
 
+sterilizedFlightAirline = flightAirline.sub(' airlines','').sub(' airways','')     
+capitalizedFlightAirline = flightAirline.split.map(&:capitalize).join(' ')
+
+def member_of_array(airlineName, transferPartners)
+    for transferPartner in transferPartners do
+        return true if transferPartner.downcase.include?(airlineName.downcase) or airlineName.downcase.include?(transferPartner.downcase)
+    end  
+
+    return false   
+end
+
+flightAlliance = false
 alliances.each {|allianceName, alliancePartners|
-    for member in alliancePartners do
-        if member.downcase.include?(flightAirline.downcase)
-            flightAlliance = allianceName
-            break
-        end     
+    if member_of_array(sterilizedFlightAirline, alliancePartners)
+        flightAlliance = allianceName
+        break
     end
 }
 
 # try points transfer to partner airlines
-allianceTransfer = false
 if flightAlliance
-    puts "#{flightAirline.capitalize} is part of #{flightAlliance}"
-    puts "Check your booking options with points from these partner airlines #{alliances[flightAlliance]}"
+    puts "\n#{capitalizedFlightAirline} is part of #{flightAlliance}.\n\nCheck these partner airlines to potentially book this flight with respective miles:"
+    for each in alliances[flightAlliance] do 
+        print each + ', '
+    end    
+    puts
     puts
 
     for alliancePartner in alliances[flightAlliance] do
-        ccPrograms.each { |cardName, transferPartners|
-            for transferPartner in transferPartners do
-                if transferPartner.downcase.include?(alliancePartner.downcase) or alliancePartner.downcase.include?(transferPartner.downcase)
-                    puts "You can transfer points to #{transferPartner} partner airline from #{cardName}" 
-                    allianceTransfer = true
-                end    
-            end  
+        cards.each { |cardName, transferPartners|
+            puts "You can convert your #{cardName} credit card points to #{alliancePartner} miles."  if member_of_array(alliancePartner,transferPartners)
         }
     end  
 else
-    puts "#{flightAirline.capitalize} is not part of an alliance"    
+    puts "#{capitalizedFlightAirline} is not a member of an alliance"    
 end     
     
 puts
 
-foundTransferPartner = false
+cards.each { |cardName, transferPartners|
+    if member_of_array(sterilizedFlightAirline, transferPartners)
+        puts "You can transfer points directly to #{capitalizedFlightAirline} from #{cardName}" 
+    else 
+        puts "You can't convert #{cardName} points directly to #{capitalizedFlightAirline} miles." 
+    end
+}    
 
-# try direct transfer
-    ccPrograms.each { |cardName, transferPartners|
-            for transferPartner in transferPartners do
-                if transferPartner.downcase.include?(flightAirline.downcase) or flightAirline.downcase.include?(transferPartner.downcase)
-                    also = 'also ' if allianceTransfer
-                    
-                    puts "You can #{also}transfer points directly to #{flightAirline.capitalize} from #{cardName}" 
+puts
+directPartners.each{|airline, partners|
+    if airline.downcase == sterilizedFlightAirline
+        puts "You can also potentially book with points from one of #{capitalizedFlightAirline} partners:\n"
+        for each in partners
+            print each + ', '
+        end    
 
-                    foundTransferPartner = true
-                end    
-            end  
-    }    
-    puts "There are no direct credit card points transfer options for #{flightAirline.capitalize}" unless foundTransferPartner
+        puts "\n\n"
+        for partner in partners do
+            cards.each { |cardName, transferPartners|
+                if member_of_array(partner, transferPartners)
+                    puts "You can transfer points directly to #{partner} from #{cardName}" 
+                else 
+                    # puts "You can't convert #{cardName} points to #{partner} miles." 
+                end
+            }    
+        end 
+    end 
+}
